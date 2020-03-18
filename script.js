dbd138df3ab58147ad40e7254341417e27885b5c
baronshell <62345252+baronshell@users.noreply.github.com>
2020-03-18 18:39:26 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+function modalshow(){
+	document.getElementById('modal').style.animationName='appear';
+	document.getElementById('modal').style.display='block';
+		document.getElementById('modal1').style.display='none';
+}
+
+function modalshow1(){
+	document.getElementById('modal1').style.animationName='appear';
+	document.getElementById('modal1').style.display='block';
+	document.getElementById('modal').style.display='none';
+}
+
+
+
+function hide(){
+		document.getElementById('modal').style.display='none';
+		document.getElementById('modal1').style.display='none';
+}
+
